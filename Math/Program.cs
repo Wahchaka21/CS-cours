4cec97c6873019e49216077ec9254a22417db03d
Wahchaka21 <quentyn.petitjean@hotmail.com>
2025-12-18 14:39:08 +0100

    ajout des \Math\ et de nouvelles boucles

---
+++ Math/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        //petit équivalent du math.random()
+        static double NextRandomRange(double minimum, double maximum)
+        {
+            Random rand = new Random();
+            return rand.NextDouble() * (maximum - minimum) + minimum;
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(NextRandomRange(1, 1000));
+            //Math.round() c'est comme en js, c'est pour arrondir à l'entier le plus proche
+            System.Console.WriteLine(Math.Round(NextRandomRange(10, 20)));
+            //retourne la plus grande valeur entre x et y
+            System.Console.WriteLine(Math.Max(5, 10));
+            //retourne la plus petite valeur entre x et y
+            System.Console.WriteLine(Math.Min(5, 10));
+            //renvoie la racine carrée de x
+            System.Console.WriteLine(Math.Sqrt(64));
+            //renvoie la valeur positive de x
+            System.Console.WriteLine(Math.Abs(-6));
+        }
+    }
+}
--- Variables_et_Boucles/Program.cs
+++ Variables_et_Boucles/Program.cs
@@ -15,62 +15,55 @@ namespace firstProgramme
             // et bool pour déclarer une varibale boolean
             bool BooleanVariable = true;
             Console.WriteLine("Salut " + String + " j'ai " + Number + " ans moi aussi et j'ai " + FloatingNumber + " points de mana " + BooleanVariable);
-
-
+            //savoir combien de lettre à un mot
+            string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            System.Console.WriteLine("la varibale txt à : " + txt.Length + " lettre");
+            //mettre en maj
+            string name = "Frieren";
+            System.Console.WriteLine(name.ToUpper());
+            System.Console.WriteLine(name.ToLower());
+            //au lieu de faire des + (concaténation) on peut faire ça :
+            string firstName = "Artoria";
+            string lastName = "Pendragon";
+            string fullName = $"je me nomme : {firstName} {lastName}";
+            System.Console.WriteLine(fullName);
+            //avoir acces à une lettre
+            string a = "Guts";
+            System.Console.WriteLine(a[1]);
+            //savoir combien de fois une lettre est répété
+            string b = "coucou";
+            System.Console.WriteLine(b.IndexOf("u"));
 
 
 
             //petite astuce, taper cw puis appuis sur tab 1 fois et ça écrit Console.WriteLine();
-            //il y a une ligne de code qui nous permet de lire la console par exemple :
-            System.Console.Write("Quel est votre nom ?"); //on va juste mettre Write
-            string leNom2 = Console.ReadLine(); //enfaite ça va ouvrir la console, et je n'aurai que à taper le nom de la personne et ça va l'assigner à la variable
-            System.Console.Write("et quel est ton âge ?");
-            string age_str = Console.ReadLine();
-            System.Console.WriteLine("donc vous vous applez " + leNom2 + " et vous avez " + age_str + " ans");
-            //int.Pars() est pour convertir une str en int car, console.ReadLine() ne renvoie que une string et non un nombre, donc on ne peut pas faire par exemple :
-            //int age = Console.ReadLine(), ça serai faux, il faudrait mettre string. Donc pour palier à ce problème on déclare d'abord la lecture de la console dans un string :
-            //string age_str = Console.ReadLine(); puis on passe cette variable à une nouvelle variable qui va remplacer le type string de age_str par un type int comme ceci :
-            int age_num = int.Parse(age_str); //par contre ça marche que si on rentre un nombre, si on decide d'écrire une string, ça plante
-            int age_prochain = age_num + 1;
-            System.Console.WriteLine("Vous aurez le 28 juin 2026 : " + age_prochain + " ans");
-
-
-
-
-
-            //mais du coup cette façon de faire n'est pas opti, car l'utilisateur pourrais très bien rentrer un age invalide ou une string, il faut donc gérer les exceptions
-            //donc comme en js, on peut faire des try et catch, donc on va recommencer une simulation :
-            System.Console.WriteLine("Quel est ton age ?");
-            string monAge = Console.ReadLine();
-            try
-            {
-                int age_int = int.Parse(monAge);
-                int add_age = age_int + 1;
-                System.Console.WriteLine("Votre age est : " + age_int);
-                System.Console.WriteLine("Vous aurez bientot : " + add_age);
-            }
-            catch
-            {
-                System.Console.WriteLine("Erreur, vous devez rentrer un age valide !");
-            }
-
-
-
-
-
-            //mais ça serai bien que par exemple si le user rentre un string ou un age invalide, au lieu de juste lui mettre le message d'erreur, ça lui redemande son age juste après
-            //donc pour ça il faut faire des boucle, voici un exemple de boucle while en C# :
+            //boucle while :
             int i = 0;
             while (i <= 10)
             {
-                System.Console.WriteLine("Valeur de i : " + i);
+                System.Console.WriteLine("Boucle while : valeur de i : " + i);
                 i++; // on peut très bien l'écrire i = i + 1 ou i += 1
             }
 
-            System.Console.WriteLine("fin de la boucle, la dernière valeur de i est : " + i);
+            System.Console.WriteLine("Boucle while : fin de la boucle, la dernière valeur de i est : " + i);
 
-            //avec ce principe, on peut reprendre l'exemple plus haut pour demander l'age de la personne :
-            //on créer bool ageValde qui commence à false, la boucle tourne tant que ageValide est false, i le Parse réussit, on met ageValide = true et la boucle s'arrête
+            //boucle for :
+            for (int j = 0; j <= 10; j++)
+            {
+                System.Console.WriteLine("Boucle for : valeur de j : " + j);
+            }
+
+            //boucle forEach, pour les tableaux
+            string[] cars = { "Ciroen", "Peugeot", "Renault" };
+            foreach (string k in cars)
+            {
+                System.Console.WriteLine(k);
+            }
+
+
+
+            //avec ce principe, on peut faire un programme pour demander l'age de la personne :
+            //on créer bool ageValde qui commence à false, la boucle tourne tant que ageValide est false, si le Parse réussit, on met ageValide = true et la boucle s'arrête
             //si le Parse échoue, on va dans le catch, on affiche l'erreur, et la boucle recommence
             int age_int2 = 0;
             bool ageValide = false;
